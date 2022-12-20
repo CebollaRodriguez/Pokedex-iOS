4756699a395591c3711b969b85891f4b081e2ee6
CebollaRodriguez <sangabo777@gmail.com>
2022-12-20 11:02:40 -0600

    Initial Commit

---
+++ Pokedex-iOS/Pokedex_iOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  Pokedex_iOSApp.swift
+//  Pokedex-iOS
+//
+//  Created by Gabriel Sanchez Peraza on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
